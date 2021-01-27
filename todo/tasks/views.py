ef4ad3e91639f34e3d2caefe4e4716b085eb6900
harshvijay123 <44875772+harshvijay123@users.noreply.github.com>
2021-01-27 17:22:21 +0530

    Add files via upload

---
+++ todo/tasks/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+# Create your views here.
+
+
+def index(request):
+    tasks=Task.objects.all()
+    form = TaskForm()
+    
+    if request.method=="POST":
+        form=TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    constext={'tasks':tasks, 'form':form}
+    return render(request,'tasks/list.html',constext)
+
+def updateTask(request,pk):
+    task=Task.objects.get(id=pk)
+    form=TaskForm(instance=task)
+    if request.method=="POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+
+
+    constext = {'form':form}
+    return render(request,"tasks/update_task.html",constext)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method=="POST":
+        item.delete()
+        return redirect("/")
+
+
+    constext={'item':item}
+    return render(request, "tasks/delete.html",constext)
